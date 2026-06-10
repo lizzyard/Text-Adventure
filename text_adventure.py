2e876c4a763d5222ff502c515c58b74d89186a64
Leila <151710793+lizzyard@users.noreply.github.com>
2026-06-09 17:25:57 -0700

    Modify items and enhance player commands
    Updated item in Town and added 'look' command for room details.

--- text_adventure.py
+++ text_adventure.py
@@ -9,33 +9,33 @@ items = {
     'Forest':'sword',
     'Cave':'shield',
     'Castle':'treasure',
-    'Town':'map'
+    'Town':'key'
 }
 
 current_room = 'Forest'
 inventory = []
-
 def show_room(room,exit_rooms,items_rooms):
-    print("What would you like to do? (ex: Forest, Town, sword)")
+    print("Commands: Town, sword, inventory, look, exit")
     print("Current room: " + room)
     print("Connected rooms: " + str(exit_rooms[room]))
     if room in items.keys():
         print("You see a " + items_rooms[room])
 
 
-
-
 def player_inventory(players_inventory):
     if not players_inventory:
         print("Inventory is empty.")
     else:
-        print("Inventory: ")
-        print(str(inventory))
+        for item in players_inventory:
+            print("-" + item)
 
 def take_item(item):
-    del items[current_room]
-    inventory.append(str(item))
-    print(item + " placed in inventory")
+    if items.get(current_room) == item:
+        del items[current_room]
+        inventory.append(str(item))
+        print(item + " placed in inventory")
+    else:
+        print("That item does not exist in this location.")
 
 def process_command(command):
     if command == "exit":
@@ -47,7 +47,10 @@ def process_command(command):
         move_player(command)
     elif command in items.values():
         take_item(command)
-
+    elif command == "look":
+        show_room(current_room,rooms,items)
+    else:
+        print("Invalid command.")
 def move_player(destination):
     global current_room
     if destination in rooms[current_room]:
@@ -56,12 +59,16 @@ def move_player(destination):
         if destination == "Castle":
             if "shield" in inventory and "sword" in inventory:
                 check_win()
+    else:
+        print("You can't go there from here.")
 
 def check_win():
     print("you won the game!")
     sys.exit()
 
+show_room(current_room,rooms,items)
+
 while True:
-    show_room(current_room,rooms,items)
     choice = input(">")
     process_command(choice)
+
